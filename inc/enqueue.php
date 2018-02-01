fc10682b6498bd911967d3ef1a7fdcad33fa014f
Kiera Howe <kiera@wileysol.com>
2018-02-01 13:26:10 -0500

    Added enqueue for fonts, async

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,16 +11,28 @@ class Enqueue {
 	public function admin_enqueue() {
 		$screen = get_current_screen();
 		if( $screen->id === 'settings_page_gfont_settings' ) { 
-			wp_enqueue_style( 'select2-style', plugin_dir_url(__FILE__) . '../vendor/select2/select2/dist/css/select2.min.css' );
-   		    wp_enqueue_script( 'select2-js',  plugin_dir_url(__FILE__) . '../vendor/select2/select2/dist/js/select2.min.js' );
+			wp_enqueue_style( 'select2-style', plugin_dir_url( __FILE__ ) . '../vendor/select2/select2/dist/css/select2.min.css' );
+   		    wp_enqueue_script( 'select2-js',  plugin_dir_url( __FILE__ ) . '../vendor/select2/select2/dist/js/select2.min.js' );
 
-   		    wp_enqueue_script( 'select-fonts-js',  plugin_dir_url(__FILE__) . '../assets/js/select_fonts.js' );
-			wp_enqueue_style( 'jfont-admin-css', plugin_dir_url(__FILE__) . '../assets/css/admin.css' );
+   		    wp_enqueue_script( 'select-fonts-js',  plugin_dir_url( __FILE__ ) . '../assets/js/select_fonts.js' );
+			wp_enqueue_style( 'jfont-admin-css', plugin_dir_url( __FILE__ ) . '../assets/css/admin.css' );
    		}
 	}
 
-	public function enqueue() { 
+	public function enqueue() {
+		$fonts = get_option( 'google_fonts' );
 
+		wp_enqueue_script( 'WebFontConfig', plugin_dir_url( __FILE__ ) . '../assets/js/gfont.js', array(), false, true  );
+		wp_localize_script( 'WebFontConfig', 'WebFontConfig', array( 'google' => array( 'families' => $fonts ) ) );
+		$script = "
+   (function(d) {
+      var wf = d.createElement('script'), s = d.scripts[0];
+      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+      wf.async = true;
+      s.parentNode.insertBefore(wf, s);
+   })(document); ";
+
+		wp_add_inline_script( 'WebFontConfig', $script, 'after' );
 	}
 }
 
